1d6326223746866bc86e33b716099d143527252e
Antoni <75241504+antoni049@users.noreply.github.com>
2023-04-26 21:46:56 +0800

    Update Function.py

--- Function.py
+++ Function.py
@@ -7,19 +7,21 @@ def GMmodel(G, risk):
 	Node = G.nodes()
 	S = np.zeros((N, N))
 	result = [0 for i in range(N)]
-	# 在全部节点中标记哪些节点为已知重要节点
+	
+	# Which nodes are marked as known important nodes among all nodes
 	vital = [0 for i in range(N)]
 	for i, num in enumerate(Node):
 		if num in risk:
 			vital[i] = 1
-	# 计算得分矩阵S
-	for i, item1 in enumerate(Node):  # 创建图G的邻接矩阵，即为a
+	
+	# Calculate the scoring matrix S
+	for i, item1 in enumerate(Node):  
 		for j, item2 in enumerate(Node):
 			if i != j:
 				S[i][j] = nx.degree(G, item1) * nx.degree(G, item2) / nx.shortest_path_length(G, source=item1, target=item2)
 			else:
 				S[i][j] = 0
-	# 计算result
+	# Calculate result
 	for i, item1 in enumerate(S):
 		for j, item2 in enumerate(S):
 			if vital[j] == 0:
@@ -28,7 +30,8 @@ def GMmodel(G, risk):
 
 # GMmodel_plus(NEGM)
 def GMmodel_plus(G, risk, vector):
-	# 计算欧式距离
+	
+	# Calculate the Euclidean distance
 	def dist(x, y):
 		res1 = np.array(x)
 		res2 = np.array(y)
@@ -38,19 +41,21 @@ def GMmodel_plus(G, risk, vector):
 	Node = G.nodes()
 	S = np.zeros((N, N))
 	result = [0 for i in range(N)]
-	# 在全部节点中标记哪些节点为已知重要节点
+	
+	# Which nodes are marked as known important nodes among all nodes
 	vital = [0 for i in range(N)]
 	for i, num in enumerate(Node):
 		if num in risk:
 			vital[i] = 1
-	# 计算得分矩阵S
-	for i, item1 in enumerate(Node):  # 创建图G的邻接矩阵，即为a
+			
+	# Calculate the scoring matrix S
+	for i, item1 in enumerate(Node):  
 		for j, item2 in enumerate(Node):
 			if i != j:
 				S[i][j] = nx.degree(G, item1) * nx.degree(G, item2) / dist(vector[i], vector[j])
 			else:
 				S[i][j] = 0
-	# 计算result
+	# Calculate the result
 	for i, item1 in enumerate(S):
 		for j, item2 in enumerate(S):
 			if vital[j] == 0:
